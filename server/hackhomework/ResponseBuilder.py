55d814709c99cfe2590b920cc0254720f6e6d953
Johan Sebastian Cortes Montenegro <johanscm50@gmail.com>
2014-10-18 20:14:04 +0100

    removed SSR and IS2 subjects

--- server/hackhomework/ResponseBuilder.py
+++ server/hackhomework/ResponseBuilder.py
@@ -6,9 +6,9 @@ class ResponseBuilder(object):
         if self.parentID == "parentID1":
             return {"tasks": [
                 {"id": 1,
-                 "subject": "IS2",
-                 "limitdate": "2015-12-31",
-                 "description": "Happy new Year pg 1",
+                 "subject": "MAT",
+                 "limitdate": "2015-06-01",
+                 "description": "Libro pág. 25, ej: 3, 4, 5",
                  "sonid": 12},
 
                 {"id": 2,
@@ -18,18 +18,6 @@ class ResponseBuilder(object):
                  "sonid": 12}]}
 
         return {"tasks": [
-            {"id": 3,
-             "subject": "IS2",
-             "limitdate": "2015-12-31",
-             "description": "Happy new Year pg 1",
-             "sonid": 11},
-
-            {"id": 4,
-             "subject": "SSR",
-             "limitdate": "2015-06-01",
-             "description": "leer el RFC 2616",
-             "sonid": 11},
-
             {"id": 5,
              "subject": "MAT",
              "limitdate": "2015-06-01",
