7a674369e4ee8414f3f3d3a250cc66dc1e0e1ec2
Juan David Vega <jdvr.93@hotmail.com>
2014-10-18 14:07:05 +0100

    Database finished

--- src/com/androidhackathongdggrancanaria/checkhomework/TaskManager.java
+++ src/com/androidhackathongdggrancanaria/checkhomework/TaskManager.java
@@ -1,10 +1,14 @@
 package com.androidhackathongdggrancanaria.checkhomework;
 
+import java.text.ParseException;
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.Locale;
 
 import android.content.ContentValues;
 import android.content.Context;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 
 public class TaskManager {
@@ -49,6 +53,52 @@ public class TaskManager {
 				+ "Date(" + dateString + ")");
 		this.close();
 	}
+	
+	public void update(Task task){
+		ContentValues values = getTaskContentValues(task);
+		this.open();
+		database.update(MySQLHelper.CHECK_TABLE_NAME, values, "_id=?", new String[]{task.get_id() + ""});
+		this.close();
+	}
+	
+	public ArrayList<Task> getTasks(){ 
+		this.open();
+		Cursor cursor = database.query(MySQLHelper.CHECK_TABLE_NAME, columns, null, null, null, null, null);
+		return getTasksFromCursos(cursor);
+	}
+
+	private ArrayList<Task> getTasksFromCursos(Cursor cursor) {
+		ArrayList<Task> res = new ArrayList<Task>();
+		cursor.moveToFirst();
+		while (cursor.isAfterLast()) {
+			res.add(this.createTaskFromCursos(cursor));
+			cursor.moveToNext();
+		}
+		cursor.close();
+		this.close();
+		return res;
+	}
+
+	private Task createTaskFromCursos(Cursor cursor) {
+		Task res = new Task();
+		res.set_id(cursor.getLong(0));
+		res.setSubject(cursor.getString(1));
+		res.setLimit(parseStringToDate(cursor.getString(2)));
+		res.setDescription(cursor.getString(3));
+		res.setSonId(cursor.getLong(4));
+		res.setDone(parseIntToBoolean(cursor.getInt(5)));
+		return res;
+	}
+
+	private Date parseStringToDate(String string) {
+		try {
+			return new SimpleDateFormat("yyyy-MM-dd").parse(string);
+		} catch (ParseException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return null;
+	}
 
 	private ContentValues getTaskContentValues(Task task) {
 		// TODO Auto-generated method stub
@@ -63,6 +113,10 @@ public class TaskManager {
 
 	private int parseBoolean(boolean done) {
 		return (done)?1:0;
+	}
+	
+	private boolean parseIntToBoolean (int done) {
+		return (done == 1)?true:false;
 	}
 	
 	private String parseDate(Date date){
