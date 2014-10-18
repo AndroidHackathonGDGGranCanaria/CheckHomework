2227b86f3d713804ef7cc46bb2c4afb21c7507b5
Johan Sebastian Cortes Montenegro <johanscm50@gmail.com>
2014-10-18 19:54:50 +0100

    changed the ID to Integers

--- server/hackhomework/ResponseBuilder.py
+++ server/hackhomework/ResponseBuilder.py
@@ -5,51 +5,51 @@ class ResponseBuilder(object):
     def build(self):
         if self.parentID == "parentID1":
             return {"tasks": [
-                {"id": "1",
+                {"id": 1,
                  "subject": "IS2",
                  "limitdate": "2015-12-31",
                  "description": "Happy new Year pg 1",
-                 "sonid": "12"},
+                 "sonid": 12},
 
-                {"id": "3",
+                {"id": 2,
                  "subject": "MAT",
                  "limitdate": "2015-06-01",
                  "description": "Libro pág. 25, ej: 3, 4, 5",
-                 "sonid": "12"}]}
+                 "sonid": 12}]}
 
         return {"tasks": [
-            {"id": "1",
+            {"id": 3,
              "subject": "IS2",
              "limitdate": "2015-12-31",
              "description": "Happy new Year pg 1",
-             "sonid": "11"},
+             "sonid": 11},
 
-            {"id": "2",
+            {"id": 4,
              "subject": "SSR",
              "limitdate": "2015-06-01",
              "description": "leer el RFC 2616",
-             "sonid": "11"},
+             "sonid": 11},
 
-            {"id": "3",
+            {"id": 5,
              "subject": "MAT",
              "limitdate": "2015-06-01",
              "description": "Libro pág. 25, ej: 3, 4, 5",
-             "sonid": "11"},
+             "sonid": 11},
 
-            {"id": "2",
+            {"id": 6,
              "subject": "LAN",
              "limitdate": "2015-06-01",
              "description": "Libro pág. 17, ej: 13a, 13b, 19",
-             "sonid": "11"},
+             "sonid": 11},
 
-            {"id": "2",
+            {"id": 7,
              "subject": "SCI",
              "limitdate": "2015-06-01",
              "description": "Libro pág. 25, ej: 3, 4, 5, 6, 7, 8, 9",
-             "sonid": "11"},
+             "sonid": 11},
 
-            {"id": "1",
+            {"id": 8,
              "subject": "ENG",
              "limitdate": "2015-12-31",
              "description": "Libro, pág. 52, ej: 8b; Libro E pág. 34 ej: 19c",
-             "sonid": "11"}]}
+             "sonid": 11}]}
